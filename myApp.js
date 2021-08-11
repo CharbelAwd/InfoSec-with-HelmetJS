2c766b5b2457bd4070d8dbb4536ac6f09d2aa5af
Charbel Awad <79443300+CharbelAwd@users.noreply.github.com>
2021-08-11 21:53:54 +0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -3,13 +3,22 @@ const app = express();
 const helmet =  require('helmet');
 app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({action: 'deny'}));
-
-
-
-
-
-
-
+app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
+app.use(helmet.ieNoOpen());
+var ninetyDaysInSeconDs = 90*24*60*60;
+app.use(helmet.hsts({
+  maxAge: ninetyDaysInSeconDs,
+  force: true
+  }))
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.noCache());
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+   defaultSrc: ["'self'"],
+   scriptSrc: ["'self'", "trusted-cdn.com"]
+  }
+}));
 
 
 
